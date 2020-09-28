0dbee5a527489d5f2b48ab770b5df4fc51cef793
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-29 02:37:44 +0530

    Updates

---
+++ precache-manifest.3cff094fac85740876ba27e097b08dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60253b5dd9e235af0494e885156b837",
+    "url": "/todoappreact/index.html"
+  },
+  {
+    "revision": "b2584e9f19702e60b219",
+    "url": "/todoappreact/static/css/main.494c06c5.chunk.css"
+  },
+  {
+    "revision": "bc319e6c0929f9af36f4",
+    "url": "/todoappreact/static/js/2.0875b169.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoappreact/static/js/2.0875b169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2584e9f19702e60b219",
+    "url": "/todoappreact/static/js/main.7c0294a0.chunk.js"
+  },
+  {
+    "revision": "69855890820e7609f62d",
+    "url": "/todoappreact/static/js/runtime-main.f3dbe82b.js"
+  }
+]);
